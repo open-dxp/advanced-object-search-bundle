c955062faf49e005b20cf00d675ac23ce0d472f6
Stefan Hagspiel <shagspiel@dachcom.ch>
2026-05-22 15:53:01 +0200

    Streamline CI workflows, update configurations, and refactor project setup

--- .github/ci/scripts/setup-opendxp-environment.sh
+++ .github/ci/scripts/setup-opendxp-environment.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-set -eu -o xtrace
+set -eu
 
-#cp -rv .github/ci/files/var .
 cp .github/ci/files/.env .
-
-# Setup composer auth
-if [ -n "$COMPOSER_AUTH" ]; then
-composer config repositories.opendxp '{"type": "composer", "url": "https://open-dxp.repo.repman.io"}' --file composer.json
-fi
